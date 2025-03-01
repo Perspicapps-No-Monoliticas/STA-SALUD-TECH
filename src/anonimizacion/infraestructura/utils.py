fcb91e35596aca10d949cb7254dc3295b4b31340
fclopez4 <fc.lopez@uniandes.edu.co>
2025-02-28 19:20:38 -0500

    feat: se agregan despachadores y consumidores

--- src/anonimizacion/infraestructura/utils.py
+++ src/anonimizacion/infraestructura/utils.py
@@ -4,11 +4,14 @@ import datetime
 
 epoch = datetime.datetime.utcfromtimestamp(0)
 
+
 def time_millis():
     return int(time.time() * 1000)
 
+
 def broker_host():
-    return os.getenv('BROKER_HOST', default="localhost")
+    return os.getenv("BROKER_HOST", default="localhost")
+
 
 def unix_time_millis(dt):
     return (dt - epoch).total_seconds() * 1000.0
