ea3bdaa09d06d758a2e546c73bae6d001a667349
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2025-03-01 15:01:57 -0500

    ingestion TOPICS

--- src/ingestion/modules/data_intake/infraestructure/constants.py
+++ src/ingestion/modules/data_intake/infraestructure/constants.py
@@ -2,3 +2,5 @@ from seedwork.infraestructure.varaibles import COUNTRY_CODE
 
 START_DATA_INGESTION_V1_TOPIC = f"{COUNTRY_CODE}-start-data-ingestion-v1"
 DATA_INGESTION_CREATED_V1_TOPIC = f"{COUNTRY_CODE}-data-ingestion-created-v1"
+DATA_INGESTION_STARTED_V1_TOPIC = f"{COUNTRY_CODE}-data-ingestion-started-v1"
+DATA_INGESTION_FINISHED_V1_TOPIC = f"{COUNTRY_CODE}-data-ingestion-finished-v1"
